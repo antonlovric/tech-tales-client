7e120c878c53a5fd26531d7724abd7768d286987
Anton Lovrić <antonlovri81@gmail.com>
2023-09-10 07:44:04 +0200

    refactor: added main tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-dark text-white`}>
         <Header></Header>
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
