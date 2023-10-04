62127d522ea17d76d8a028b891eaeed3e5583923
Anton Lovrić <antonlovri81@gmail.com>
2023-10-04 23:21:43 +0200

    feat: search posts
    - search query is added to url
    - added pending state

--- app/(authenticated-pages)/posts/page.tsx
+++ app/(authenticated-pages)/posts/page.tsx
@@ -1,7 +1,16 @@
+import PostsSearch from '@/app/components/PostsSearch';
 import React from 'react';
 
 const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <div className="w-10/12 mx-auto">
+      <h1 className="text-center text-6xl font-semibold mb-2">Tech Tales</h1>
+      <h2 className="text-center text-3xl font-extralight">
+        Today&apos;s stories for tomorrow&apos;s tech
+      </h2>
+      <PostsSearch />
+    </div>
+  );
 };
 
 export default Posts;
---
+++ app/components/PostsSearch.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+import React, { FormEvent, useTransition } from 'react';
+
+const PostsSearch = () => {
+  const [isPending, setTransition] = useTransition();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget as HTMLFormElement);
+    const searchQuery = data.get('search')?.toString();
+    const params = new URLSearchParams(window.location.search);
+    if (searchQuery) {
+      params.set('search', searchQuery);
+    } else params.delete('search');
+    setTransition(() => {
+      replace(`${pathname}?${params.toString()}`);
+    });
+  };
+
+  return (
+    <form
+      className="mt-4 grid grid-cols-8 w-10/12 mx-auto gap-2"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="search"
+        placeholder="Start your journey"
+        className="bg-dark-gray focus:outline-none border-blog px-3 py-2 col-span-7 rounded-md"
+      />
+      <button type="submit" className="bg-blog-blue px-3 py-2 rounded-md">
+        {isPending ? 'Loading' : 'Search'}
+      </button>
+    </form>
+  );
+};
+
+export default PostsSearch;
