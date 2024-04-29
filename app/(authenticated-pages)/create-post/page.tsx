7b9458d69adae0c3947e9f333099ebead4d8230f
Anton Lovrić <antonlovri81@gmail.com>
2024-04-30 00:45:30 +0200

    feat(post): added basic post creation functionality

--- app/(authenticated-pages)/create-post/page.tsx
+++ app/(authenticated-pages)/create-post/page.tsx
@@ -1,10 +1,44 @@
 import TextEditor from '@/app/components/TextEditor';
+import { prisma } from '@/app/helpers/api';
+import { getActiveUser } from '@/app/helpers/auth';
+import { JSONContent } from '@tiptap/react';
 import React from 'react';
 
-const CreatePost = () => {
+export interface ICreatePostRequest {
+  html_content: string;
+  json_content: JSONContent;
+  categoryIds: number[];
+  title: string;
+  summary: string;
+}
+
+const CreatePost = async () => {
+  const categories = await prisma.categories.findMany();
+
+  async function handleCreate(props: ICreatePostRequest) {
+    'use server';
+    const activeUser = getActiveUser();
+    if (activeUser?.id) {
+      await prisma.posts.create({
+        data: {
+          html_content: props.html_content,
+          json_content: props.json_content,
+          summary: props.summary,
+          title: props.title,
+          post_categories: {
+            create: props.categoryIds.map((categoryId) => ({
+              categories_id: categoryId,
+            })),
+          },
+          users_id: activeUser.id,
+        },
+      });
+    }
+  }
+
   return (
     <div>
-      <TextEditor />
+      <TextEditor categories={categories} createPost={handleCreate} />
     </div>
   );
 };
--- app/components/TextEditor.tsx
+++ app/components/TextEditor.tsx
@@ -1,9 +1,16 @@
 'use client';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import React from 'react';
+import React, { useState } from 'react';
+import type { categories } from '@prisma/client';
+import { ICreatePostRequest } from '../(authenticated-pages)/create-post/page';
 
-const TextEditor = () => {
+interface ITextEditor {
+  categories?: categories[];
+  createPost: (args: ICreatePostRequest) => void;
+}
+
+const TextEditor = ({ categories, createPost }: ITextEditor) => {
   const titleEditor = useEditor({
     extensions: [StarterKit],
     content: '<h1>This is the title of your article!</h1>',
@@ -14,8 +21,36 @@ const TextEditor = () => {
     content: '<p>Hello World! 🌎️</p>',
     injectCSS: false,
   });
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    createPost({
+      categoryIds: selectedCategories,
+      html_content: bodyEditor?.getHTML() || '',
+      json_content: bodyEditor?.getJSON() || { type: '', content: [] },
+      summary: '',
+      title: titleEditor?.getHTML() || '',
+    });
+  };
+
+  function selectCategory(id: number) {
+    const updatedCategories = selectedCategories.concat([id]);
+    setSelectedCategories(updatedCategories);
+  }
+
+  function removeCategory(targetId: number) {
+    setSelectedCategories(selectedCategories.filter((id) => id !== targetId));
+  }
+
+  function toggleCategory(id: number) {
+    if (getIsCategorySelected(id)) {
+      removeCategory(id);
+    } else selectCategory(id);
+  }
+
+  function getIsCategorySelected(targetIid: number) {
+    return !!selectedCategories.find((id) => id === targetIid);
+  }
 
   function toggleBold() {
     bodyEditor?.chain().focus().toggleBold().run();
@@ -25,6 +60,21 @@ const TextEditor = () => {
     <main>
       <EditorContent editor={titleEditor} className="text-4xl" />
       <hr className="mt-2 mb-6" />
+      <div className="flex justify-end items-center gap-2">
+        {categories?.map((category) => (
+          <div
+            key={category.id}
+            onClick={() => toggleCategory(category.id)}
+            className={`${
+              !getIsCategorySelected(category.id)
+                ? 'bg-none border-blog-blue border'
+                : 'bg-blog-blue border border-transparent'
+            }  flex items-center justify-center px-2 py-1 rounded-md text-xs cursor-pointer`}
+          >
+            {category.name}
+          </div>
+        ))}
+      </div>
       <div className="flex items-center gap-2">
         <button onClick={toggleBold}>Bold</button>
       </div>
