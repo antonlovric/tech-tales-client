df68355252c36138fe6a807f4709c3b548d53685
Anton Lovrić <antonlovri81@gmail.com>
2024-05-20 22:35:38 +0200

    style(post-details): reorganized to post details layout

--- app/(authenticated-pages)/post/[postId]/page.tsx
+++ app/(authenticated-pages)/post/[postId]/page.tsx
@@ -18,24 +18,29 @@ const Post = async ({ params }: IPostPage) => {
   const sanitizedBody = DOMPurify.sanitize(post?.html_content || '');
 
   return (
-    <main className="flex flex-col gap-3">
+    <main className="max-w-[400px] md:max-w-[1000px] mx-auto">
       <div
-        className="text-xl text-center"
         dangerouslySetInnerHTML={{ __html: sanitizedTitle }}
+        className="text-4xl font-semibold text-center mb-5"
       ></div>
-      <div className="flex justify-center flex-col items-center w-[600px] mx-auto">
-        <img src={post?.cover_image || ''} alt="Post Cover Image" width={600} />
-        <div className="flex items-center gap-2 justify-between w-full">
-          <p>
-            {post?.author.first_name} {post?.author.last_name}
-          </p>
-          <p>{formatDate(post?.created_at)}</p>
-        </div>
-      </div>
       <div
-        className="mb-5"
         dangerouslySetInnerHTML={{ __html: sanitizedSummary }}
+        className="mb-5"
       ></div>
+      <section className="flex flex-col gap-5 mb-5 w-3/4 mx-auto">
+        <div>
+          <div className="flex items-center gap-2">
+            <div>{/* profile image */}</div>
+            <div className="flex flex-col justify-between">
+              <p>
+                Author: {post?.author.first_name} {post?.author.last_name}
+              </p>
+              <p>{formatDate(post?.created_at)}</p>
+            </div>
+          </div>
+        </div>
+        <img src={post?.cover_image || ''} alt="Post cover image" />
+      </section>
       <div dangerouslySetInnerHTML={{ __html: sanitizedBody }}></div>
     </main>
   );
