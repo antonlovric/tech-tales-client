c7acc0cf98381fa9481975f9a64b949a9c5afcb5
Anton Lovrić <antonlovri81@gmail.com>
2024-08-27 20:05:33 +0200

    feat(global): added route loading spinner

---
+++ app/(authenticated-pages)/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
